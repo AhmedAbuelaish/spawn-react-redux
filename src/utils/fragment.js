750bdf76427c1d346b4ef9e117fec845d5b93757
fatherofbread <ahmedabuelaish@hotmail.com>
2019-03-07 00:09:08 -0600

    dist score, total score

--- src/utils/fragment.js
+++ src/utils/fragment.js
@@ -1,39 +1,52 @@
 function createFragmentedArray(oldArray) {
 	var newArray = oldArray.map((currentItem, index) => {
-		let parentRadius = currentItem.radius
+        let parentRadius = currentItem.radius
+        
+
 		let parentX = currentItem.coordX
 		let parentY = currentItem.coordY
 	})
 }
 
-function distributeValue(startValue, minValue, distFactor) {
-	// <1 distFactor for decreasing sizes but greater quantity (0.2 min)
+function distributeValue(startValue, settings) {
+    // settings is an object containing minValue & distFactor
+    
+    // <1 distFactor for decreasing sizes but greater quantity (0.2 min)
 	// >1 distFactor for chances of increasing sizes but fewer quantity
-	if (!distFactor) {
-		distFactor = 1
+	if (!settings.distFactor) {
+		settings.distFactor = 1
 	}
-	if (distFactor < 0.2) {
-		distFactor = 0.2
+	if (settings.distFactor < 0.2) {
+		settings.distFactor = 0.2
     }
     
 	let remainder = startValue
 	let currentValue = 0
-	let resultArray = []
-	let processSteps = 0
+    let resultArray = []
+    
+    // Checks:
+    let processSteps = 0
+    let retotalizer = 0
 
-	if (startValue <= minValue) {
+	if (startValue <= settings.minValue) {
 		return resultArray
 	} else {
-		while (remainder >= minValue) {
+		while (remainder >= settings.minValue) {
 			processSteps += 1
-			currentValue = Math.trunc(remainder * Math.random() * distFactor * 1000) / 1000
-			if (currentValue >= minValue) {
-				resultArray.push(currentValue)
+			currentValue = Math.trunc(remainder * Math.random() * settings.distFactor * 1000) / 1000
+			if (currentValue >= settings.minValue) {
+                resultArray.push(currentValue)
+                retotalizer += currentValue
 			}
 			remainder -= currentValue
 		}
 	}
-	console.log(Math.trunc((100 * resultArray.length) / processSteps), '% distribution score')
-	console.log(resultArray)
+    console.log(Math.trunc((100 * resultArray.length) / processSteps), '% distribution score')
+    console.log(Math.trunc((100 * retotalizer) / startValue), '% total score')
+	// console.log(resultArray)
 	return resultArray
 }
+
+
+
+distributeValue (10, {distFactor: 0.5, minValue: 0.2})
