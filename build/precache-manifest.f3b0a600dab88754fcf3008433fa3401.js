45cec92b5053a53413329d9fa99daa405ea44cc3
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-23 11:49:59 -0500

    update homepage

---
+++ build/precache-manifest.f3b0a600dab88754fcf3008433fa3401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bc3e1298ff6be272931",
+    "url": "/spawn-react-redux/static/js/runtime~main.2bc3e129.js"
+  },
+  {
+    "revision": "a3d45327b1c4d9acd157",
+    "url": "/spawn-react-redux/static/js/main.a3d45327.chunk.js"
+  },
+  {
+    "revision": "8be8439f9b9533ffbd28",
+    "url": "/spawn-react-redux/static/js/2.8be8439f.chunk.js"
+  },
+  {
+    "revision": "a3d45327b1c4d9acd157",
+    "url": "/spawn-react-redux/static/css/main.c7f7f151.chunk.css"
+  },
+  {
+    "revision": "4655918785dd7c0b0866acfdbb93eb24",
+    "url": "/spawn-react-redux/index.html"
+  }
+];
