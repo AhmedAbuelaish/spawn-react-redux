eb80ba891c884bd7a9a97e3b6e100076d8db5f3a
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-23 11:42:50 -0500

    add gh-pages to dependencies

---
+++ build/precache-manifest.3177703f4a1421a9a195e1d7593d3996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bc3e1298ff6be272931",
+    "url": "/spawn-react-redux/static/js/runtime~main.2bc3e129.js"
+  },
+  {
+    "revision": "71e4c0354cfbc6dac3b4",
+    "url": "/spawn-react-redux/static/js/main.71e4c035.chunk.js"
+  },
+  {
+    "revision": "8be8439f9b9533ffbd28",
+    "url": "/spawn-react-redux/static/js/2.8be8439f.chunk.js"
+  },
+  {
+    "revision": "71e4c0354cfbc6dac3b4",
+    "url": "/spawn-react-redux/static/css/main.c7f7f151.chunk.css"
+  },
+  {
+    "revision": "73c4bcfe06522b7fc582ad5607dcd763",
+    "url": "/spawn-react-redux/index.html"
+  }
+];
