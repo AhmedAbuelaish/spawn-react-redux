b76b392bfefa67d2d6c5660759f5e6d338dd9218
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-28 21:21:34 -0500

    Updates

--- precache-manifest.cd775e7d172f0ba0127cb808a2650b9b.js
+++ precache-manifest.cd775e7d172f0ba0127cb808a2650b9b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/spawn-react-redux/static/js/runtime~main.2bc3e129.js"
   },
   {
-    "revision": "4d8095b949019aefb553",
-    "url": "/spawn-react-redux/static/js/main.4d8095b9.chunk.js"
+    "revision": "b1b3f4068a9e7a57488d",
+    "url": "/spawn-react-redux/static/js/main.b1b3f406.chunk.js"
   },
   {
     "revision": "f67bab2adef4c998946b",
     "url": "/spawn-react-redux/static/js/2.f67bab2a.chunk.js"
   },
   {
-    "revision": "4d8095b949019aefb553",
+    "revision": "b1b3f4068a9e7a57488d",
     "url": "/spawn-react-redux/static/css/main.779a0659.chunk.css"
   },
   {
-    "revision": "2b2ce25a3f0385c678aecaeca7fd9562",
+    "revision": "75783df21a720ce9cd159c22b7c20c05",
     "url": "/spawn-react-redux/index.html"
   }
 ];
