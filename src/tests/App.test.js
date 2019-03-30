37d6786481d5ac6f72eafc28e5d430bc96d88dae
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-30 12:08:33 -0500

    define store in test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 const store = createStore(shapeReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
