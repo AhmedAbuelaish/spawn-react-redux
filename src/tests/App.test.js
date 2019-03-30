9e576a1aef108887290a78ef0a5b8dc421d9ba26
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-30 10:55:22 -0500

    added provider to App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../components/App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+	const div = document.createElement('div')
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		div
+	)
+	ReactDOM.unmountComponentAtNode(div)
+})
