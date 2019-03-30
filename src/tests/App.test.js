f14027ea2e5822ff42e1b2a138181809e08a7d8a
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-03-30 11:13:17 -0500

    createStore in test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from '../components/App'
 import { Provider } from 'react-redux'
 
+const store = createStore(shapeReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 it('renders without crashing', () => {
 	const div = document.createElement('div')
 	ReactDOM.render(
