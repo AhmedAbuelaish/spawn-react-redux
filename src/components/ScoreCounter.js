0f13d42ee9ab196f051260b076785c23d949ae03
AhmedAbuelaish <ahmedabuelaish@hotmail.com>
2019-04-02 16:41:30 -0500

    commit score counter

--- src/components/ScoreCounter.js
+++ src/components/ScoreCounter.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class ScoreCounter extends Component {
-    
-    handleClick = () => {
-        this.props.getWinningPath()
-    }
+	handleClick = () => {
+		this.props.getWinningPath()
+	}
 
-  render() {
-    return (
-        <div className='score' onClick={this.handleClick}>
-        Score: {this.props.nodes.length-1}
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='score' onClick={this.handleClick}>
+				Score: {this.props.nodes.length - 1}
+			</div>
+		)
+	}
 }
 
-
 const mapStateToProps = state => ({
-	nodes: state.nodes,
+	nodes: state.nodes
 })
 
 const mapDispatchToProps = dispatch => ({
-    getWinningPath: () => dispatch({ type: 'GET_WINNING_PATH' }),
+	getWinningPath: () => dispatch({ type: 'GET_WINNING_PATH' })
 })
 
 export default connect(
